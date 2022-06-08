4e962ba692841317da1669f8ba5b5c1d67fd91bf
Milraga19 <raden.ijlal@gmail.com>
2022-06-08 21:09:50 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
+import './css/Style.css';
+import Awal from './components/Awal';
+import mobileNav from './components/mobileNav';
+import Pesan from './components/Pesan';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='atas'>
+        <NavigationBar />
+        <mobileNav />
+        <Awal />
+      </div>
+      {/* <div className='kedua'>
+        <Pesan />
+      </div> */}
     </div>
+    
   );
 }
 
